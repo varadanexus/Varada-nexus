4779a481424564c2e3029d57db64defaa243f49b
varadanexus <varadanexus@gmail.com>
2026-03-18 22:45:50 +0530

    Create global-logout.js

---
+++ global-logout.js
@@ -0,0 +1,21 @@
+async function logout(){
+
+try{
+
+const client = supabase.createClient(
+"https://ticsgbtxfhhihamejiss.supabase.co",
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpY3NnYnR4ZmhoaWhhbWVqaXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MjE5MjksImV4cCI6MjA4ODk5NzkyOX0.rWgLPUMNnHIouP4ANQYfmzr3jAopfd3AFouoAMhSkmg"
+)
+
+await client.auth.signOut()
+
+}catch(e){
+console.log("Logout error:",e)
+}
+
+localStorage.clear()
+sessionStorage.clear()
+
+window.location.href="login.html"
+
+}

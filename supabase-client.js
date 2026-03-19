d44df264dda02936d42fc7f679b8337b41eb4c50
varadanexus <varadanexus@gmail.com>
2026-03-19 09:17:13 +0530

    Create supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ticsgbtxfhhihamejiss.supabase.co"
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpY3NnYnR4ZmhoaWhhbWVqaXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MjE5MjksImV4cCI6MjA4ODk5NzkyOX0.rWgLPUMNnHIouP4ANQYfmzr3jAopfd3AFouoAMhSkmg"
+
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+)

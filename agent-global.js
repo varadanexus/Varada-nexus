087069b2c348c6c6f7f306c73c2be08829542e6f
varadanexus <varadanexus@gmail.com>
2026-03-24 15:35:11 +0530

    Update agent-global.js

--- agent-global.js
+++ agent-global.js
@@ -1,5 +1,26 @@
 /* 🔥 AGENT GLOBAL FILE */
+async function getAgentId(){
 
+  const {data:{session}} = await supabaseClient.auth.getSession()
+
+  if(!session){
+    window.location = "login.html"
+    return null
+  }
+
+  const {data,error} = await supabaseClient
+  .from("commission_agents")
+  .select("id,name")
+  .eq("auth_id", session.user.id)
+  .single()
+
+  if(error || !data){
+    alert("Agent not found")
+    return null
+  }
+
+  return data
+}
 /* ENSURE GLOBAL */
 window.logout = async function(){
 

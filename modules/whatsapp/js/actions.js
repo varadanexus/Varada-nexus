66bdd7b763bf41f84e7529cce185985f70f8b43a
varadanexus <varadanexus@gmail.com>
2026-05-07 14:55:27 +0530

    Create actions.js

---
+++ modules/whatsapp/js/actions.js
@@ -0,0 +1,23 @@
+window.sendTripUpdate = async function(){
+
+  alert(
+    "Next step:\nConnect trip selection modal"
+  )
+
+}
+
+window.sendPaymentReminder = async function(){
+
+  alert(
+    "Next step:\nConnect payment reminder flow"
+  )
+
+}
+
+window.sendStatement = async function(){
+
+  alert(
+    "Next step:\nConnect statement PDF flow"
+  )
+
+}
